4bf4c580cdb882ccc08ed09ab44fd5a129855b1d
hoseintoghanian <toghanianh1382@yahoo.com>
2023-05-31 21:20:52 +0330

    login and controller changes

--- src/main/java/com/example/shopapplication/Controller.java
+++ src/main/java/com/example/shopapplication/Controller.java
@@ -5,20 +5,28 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class Controller {
     private Stage stage;
     private Scene scene;
     private FXMLLoader fxmlLoader;
 
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int CAPTCHA_LENGTH = 6;
+    private String captchaText;
 
-    public void changeScene(ActionEvent e,String fxml) throws IOException {
-        fxmlLoader = new FXMLLoader(Application.class.getResource(fxml));
+
+    public void switchToScene1(ActionEvent e) throws IOException {
+        fxmlLoader = new FXMLLoader(Application.class.getResource("login.fxml"));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
@@ -26,11 +34,15 @@ public class Controller {
     }
 
     public void switchToScene2(ActionEvent e) throws IOException {
-        fxmlLoader = new FXMLLoader(Application.class.getResource("signUp.fxml"));
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+        if (txtCaptchaInput.getText().equals(captchaText)) {
+            fxmlLoader = new FXMLLoader(Application.class.getResource("signUp.fxml"));
+            stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+            scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            txtCaptchaInput.clear();
+        }
     }
 
 
@@ -46,4 +58,25 @@ public class Controller {
             txtApplicant.setText(buttonCustomer.getText());
         }
     }
+
+    @FXML
+    private Label txtCaptcha;
+    @FXML
+    private TextField txtCaptchaInput;
+
+    public void captcha() throws Exception {
+        captchaText = generateCaptchaText();
+
+        txtCaptcha.setText(captchaText);
+    }
+
+    private String generateCaptchaText() {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(CAPTCHA_LENGTH);
+        for (int i = 0; i < CAPTCHA_LENGTH; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
 }
