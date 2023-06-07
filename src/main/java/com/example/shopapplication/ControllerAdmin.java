9fb51b60945cc114774620c5309b226f3d3ced31
hoseintoghanian <toghanianh1382@yahoo.com>
2023-06-08 00:32:01 +0330

    define properties in CotrollerAdmin

--- src/main/java/com/example/shopapplication/ControllerAdmin.java
+++ src/main/java/com/example/shopapplication/ControllerAdmin.java
@@ -1,4 +1,25 @@
 package com.example.shopapplication;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ControllerAdmin {
+
+    @FXML
+    private TableView<ControllerAdmin> TVBankAccount;
+    @FXML
+    private TableColumn<ControllerAdmin, String> TCBankName;
+    @FXML
+    private TableColumn<ControllerAdmin, Integer> TCBranch;
+    @FXML
+    private TableColumn<ControllerAdmin,Integer> TCAccountNumber;
+    @FXML
+    private TableColumn<ControllerAdmin,String> TCAccountType;
+    @FXML
+    private TableColumn<ControllerAdmin,String> TCBalance;
+    @FXML
+    private TableColumn<ControllerAdmin,String> TCOpeningDate;
+
+
 }
