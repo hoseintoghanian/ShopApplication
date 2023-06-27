93e0f789e27b3c5ecad44f3fb0cb178356d5ee1e
Hossein <michaelfaho@gmail.com>
2023-06-27 18:24:05 +0330

    controllerProduct change chat background

--- src/main/java/com/example/shopapplication/ControllerProduct.java
+++ src/main/java/com/example/shopapplication/ControllerProduct.java
@@ -1,4 +1,48 @@
 package com.example.shopapplication;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ControllerProduct {
+
+
+    @FXML
+    private ImageView chatBackgroundImg;
+    @FXML
+    private ImageView imgTheme1, imgTheme2, imgTheme3, imgTheme4, imgTheme5, imgTheme6, imgTheme7;
+
+    public void setChatBackground() {
+
+        Image t1 = new Image("t1.png");
+        Image t2 = new Image("t2.png");
+        Image t3 = new Image("t3.png");
+        Image t4 = new Image("t4.png");
+        Image t5 = new Image("t5.png");
+        Image t6 = new Image("t6.png");
+        Image t7 = new Image("t7.png");
+
+        imgTheme1.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t1);
+        });
+        imgTheme2.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t2);
+        });
+        imgTheme3.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t3);
+        });
+        imgTheme4.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t4);
+        });
+        imgTheme5.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t5);
+        });
+        imgTheme6.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t6);
+        });
+        imgTheme7.setOnMouseClicked(ev -> {
+            chatBackgroundImg.setImage(t7);
+        });
+    }
+
 }
