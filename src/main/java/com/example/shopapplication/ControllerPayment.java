c1f6a55e0109594d8e47ed9e3280ee87ac4c2726
hoseintoghanian <toghanianh1382@yahoo.com>
2023-06-30 00:05:14 +0330

    changes on controllerpayment

--- src/main/java/com/example/shopapplication/ControllerPayment.java
+++ src/main/java/com/example/shopapplication/ControllerPayment.java
@@ -2,9 +2,16 @@ package com.example.shopapplication;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class ControllerPayment {
@@ -15,8 +22,23 @@ public class ControllerPayment {
     private TextField txtcaptchainput2;
     @FXML
     private Label txtCaptcha2;
-    public void changeScene(ActionEvent e, String fxml) {
 
+    public void ChangeScene2(ActionEvent e,String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(fxml));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void ChangeToCartScene(ActionEvent e) throws IOException {
+        ChangeScene2(e, "cart.fxml");
+    }
+    public void changeToBakScene(ActionEvent e) throws IOException {
+        ChangeScene2(e, "bankPortal.fxml");
+    }
+    public void changeToPaymentScene(ActionEvent e) throws IOException{
+        ChangeScene2(e,"payment.fxml");
     }
 
     public void captcha2() throws Exception {
@@ -32,8 +54,15 @@ public class ControllerPayment {
         }
         return sb.toString();
     }
-
-
+    @FXML
+    private Label labelfinalcost;
+    public void displayInfo(){
+        int sum = 0;
+        for (int i=0;i<Application.shop.currentCustomer.items.size();i++){
+            sum+=Application.shop.currentCustomer.items.get(i).price;
+        }
+        labelfinalcost.setText(String.valueOf(sum));
+    }
     public void buy() {
         System.out.println(2);
     }
