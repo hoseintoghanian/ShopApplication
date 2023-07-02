5acdf951be8061bdbb5269825cfbe1923f04e1e2
Alireza <alirezanejatiz27@gmail.com>
2023-07-03 00:49:38 +0330

    add javafx.scene.media Package and ProjectSound.java class to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.example.shopapplication {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jfxrt;
 
 
     opens com.example.shopapplication to javafx.fxml;
     exports com.example.shopapplication;
+    exports com.company.java;
+    opens com.company.java to javafx.fxml;
 }
