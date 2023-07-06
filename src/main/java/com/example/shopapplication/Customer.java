b1db4fd4f156a12d2bd70c8cec58ddcb001b567c
hoseintoghanian <toghanianh1382@yahoo.com>
2023-07-06 17:51:39 +0330

    change Customer.java

--- src/main/java/com/example/shopapplication/Customer.java
+++ src/main/java/com/example/shopapplication/Customer.java
@@ -1,18 +1,26 @@
 package com.example.shopapplication;
 
+import javafx.scene.image.Image;
+
+import java.io.File;
 import java.util.*;
 
 public class Customer extends Applicant {
-    long wallet = 1000;
-    long increaseAmount;
+
+    public long wallet;
+    public long increaseAmount = 0;
+    ArrayList<Item> AuctionItems = new ArrayList<>();
     ArrayList<Item> cartItems = new ArrayList<>();
     ArrayList<Item> purchase = new ArrayList<>();
 
     //yek arraylist baraye buy history bezan
     ArrayList<String> discountCode;//it can be other data structures instead of arraylist
 
-    public Customer(String firstname, String lastname, String phoneNumber, String username, String password, String email) {
+    public Customer(String firstname, String lastname, String phoneNumber, String username, String password, String email, Long wallet, String imageurl) {
         super(firstname, lastname, phoneNumber, username, password, email);
+        this.wallet=wallet;
+        //File file = new File(imageurl);
+        this.image=new Image(imageurl);
         applicantKind = "customer";
     }
 
