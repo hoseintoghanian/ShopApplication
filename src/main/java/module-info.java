7be6de73f6be6d520ddf7c246c2fca7d1484ab45
Hossein <michaelfaho@gmail.com>
2023-07-07 16:22:55 +0330

    add libraries

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.shopapplication {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires java.mail;
 
     opens com.example.shopapplication to javafx.fxml;
     exports com.example.shopapplication;
