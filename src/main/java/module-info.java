acc9a600517da7cfe150eb225334035217de5fbf
Hossein Ajezi <michaelfoho@gmail.com>
2023-10-02 14:40:19 +0330

    the last

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.shopapplication {
     requires java.sql;
     requires java.desktop;
     requires java.mail;
+    //requires jfxrt;
 
     opens com.example.shopapplication to javafx.fxml;
     exports com.example.shopapplication;
